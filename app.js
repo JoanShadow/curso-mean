6b7294f143dad3fd5e9d05d4334af291e7ac2130
JoanShadow <joanpecamp1993@gmail.com>
2017-11-06 15:17:18 +0100

    principal

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// Cargar rutas
+var user_routes = require('./routes/user');
+var artist_routes = require('./routes/artist');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// Configurar cabeceras http
+
+// Rutas base
+app.use('/api', user_routes);
+app.use('/api', artist_routes);
+
+/*app.get('/pruebas', function(req, res) {
+	res.status(200).send({message: 'Bienvenido al curso'});
+	});*/
+
+module.exports = app;
