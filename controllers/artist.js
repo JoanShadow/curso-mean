c011d4f0f8879edb203edd229caa7842201d79f4
JoanShadow <joanpecamp1993@gmail.com>
2017-11-08 12:36:18 +0100

    album y +

--- controllers/artist.js
+++ controllers/artist.js
@@ -126,10 +126,55 @@ function deleteArtist(req, res) {
 	});
 }
 
+function uploadImage(req, res) {
+	var artistId = req.params.id;
+	var file_name = 'No subido..';
+
+	if(req.files) {
+		var file_path = req.files.image.path;
+		var file_split = file_path.split('\\');
+		var file_name = file_split[2];
+
+		var ext_split = file_name.split('\.');
+		var file_ext = ext_split[1];
+
+		console.log(ext_split);
+		if(file_ext == 'png' || file_ext == 'jpg' || file_ext == 'gif') {
+
+			Artist.findByIdAndUpdate(artistId, {img: file_name}, (err, artistUpdated) => {
+				if(!artistUpdated) {
+					res.status(404).send({message: 'No se ha podido actualizar la imagen del artista'});
+			}else {
+				res.status(200).send({artist: artistUpdated});
+			}
+			});
+		}else {
+			res.status(200).send({message: 'Extensión del archivo no válida'});
+		}
+	}else {
+		res.status(200).send({message: 'No has subido ninguna imagen..'});
+	}
+}
+
+function getImageFile(req, res) {
+	var imageFile = req.params.imageFile;
+	var path_file = './uploads/artists/'+imageFile;
+
+	fs.exists(path_file, function(exists) {
+		if(exists) {
+			res.sendFile(path.resolve(path_file));
+		}else {
+			res.status(200).send({message: 'No existe la imagen..'});
+		}
+	});
+}
+
 module.exports = {
 	getArtist,
 	getArtists,
 	saveArtist,
 	updateArtist,
-	deleteArtist
+	deleteArtist,
+	uploadImage,
+	getImageFile
 };
