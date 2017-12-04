77a294c0d2e05fee0adc1f6eea4333739608c839
JoanShadow <joanpecamp1993@gmail.com>
2017-12-04 18:24:42 +0100

    m

--- api/middlewares/authenticated.js
+++ api/middlewares/authenticated.js
@@ -1,9 +1,9 @@
 'use strict'
- 
+
 var jwt = require('jwt-simple');
 var moment = require('moment');
 var secret = 'clave_secreta_curso';
- 
+
 exports.ensureAuth = function(req, res, next){
     if(!req.headers.authorization){
         return res.status(403).send({message: 'La petición no tiene la cabecera de autenticación'});
